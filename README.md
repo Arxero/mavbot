3335686cbd5d61579273e439a337ba67a51fcb9c
Maverick <feruchio599@gmail.com>
2022-05-28 12:36:16 +0300

    clean start

---
+++ README.md
@@ -0,0 +1,3 @@
+Simple discord bot that will serve the need to show info about the cs server with the command `++acfun`
+
+[Repo link](https://github.com/Arxero/mavbot)
--- commands/say.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'say',
-	description: 'Impersonating the bot',
-	execute(message, args) {
-        message.delete(1)
-        message.channel.send(args.join(' '))
-	},
-};
